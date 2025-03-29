324faee70b97d2af4fae8bf2f9a94bb300e868c9
NarongritSrikhua <beemonkeysrikhua@gmail.com>
2025-03-29 19:58:36 +0700

    add: HomeClient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import HomeClient from "./pageClient";
+
 export default function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <HomeClient/>
   );
 }
---
+++ src/app/pageClient.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const HomeClient = () => {
+    return (
+        <div>
+            Word Book
+        </div>
+    )
+}
+
+export default HomeClient
